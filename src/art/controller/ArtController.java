cbd34a1d24a6f951853fbe80239922b66d722c50
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-05-01 10:41:16 -0600

    Made some changes in controller to fix errors in canvas.

--- src/art/controller/ArtController.java
+++ src/art/controller/ArtController.java
@@ -1,9 +1,30 @@
 package art.controller;
 
+import java.io.IOException;
+
+import art.view.ArtFrame;
+
 public class ArtController 
 {
+	private ArtFrame frame;
+	
+	public ArtController()
+	{
+		
+	}
+	
 	void start()
 	{
 		
+	}
+	
+	public ArtFrame getFrame()
+	{
+		return frame;
+	}
+	
+	public void handleErrors(IOException error)
+	{
+		System.out.println(error);
 	}
 }
